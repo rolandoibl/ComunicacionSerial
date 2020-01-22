7d854ba2dfeb06982b63f8155536a047f475c0c8
rolandoibl <rolandoil@hotmail.com>
2020-01-22 16:25:02 -0600

    Ejemplo de comunicación serial en C# con interfaz gráfica sencilla

---
+++ ejemplo_comunicacionSerial/ejemplo_comunicacionSerial/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ejemplo_comunicacionSerial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tmrAdquisicion = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(366, 65);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(102, 38);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(366, 126);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(102, 38);
+            this.btnDetener.TabIndex = 1;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // tmrAdquisicion
+            // 
+            this.tmrAdquisicion.Interval = 200;
+            this.tmrAdquisicion.Tick += new System.EventHandler(this.tmrAdquisicion_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 270);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer tmrAdquisicion;
+    }
+}
+
---
+++ ejemplo_comunicacionSerial/ejemplo_comunicacionSerial/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+//El siguiente código implementa la comunicación serial y escritura de un DataGriedView con los datos recibidos
+//en tiempo real desde un sensor de Arduino, falta implementar la escritura e importación de archivoc ".csv" 
+
+//22
+namespace ejemplo_comunicacionSerial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //Se crea nuestro objeto tipo SerialPort el cual guardara los datos recibidos de nuestro puerto serial
+        SerialPort serialPort = new SerialPort();
+
+
+        /// <summary>
+        /// Función que nos provee de las propiedades del puerto serial y la lectura de datos del puerto
+        /// </summary>
+        public void ComunicacionPuertoSerie()
+        {
+            // Asignamos las propiedades
+            serialPort.BaudRate = 9600;  //Velocidad en Baudios
+            serialPort.PortName = "COM5";  //Nombre del puerto
+
+            // Creamos el evento para recibir datos
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+
+            // Controlamos que el puerto indicado esté operativo
+            try
+            {
+                // Abrimos el puerto serie
+                serialPort.Open();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        /// <summary>
+        /// Evento que inicia el recibo de datos del puerto serial
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Obtenemos el puerto serie que lanza el evento
+            //SerialPort currentSerialPort = (SerialPort)sender;
+
+            // Leemos el dato recibido del puerto serie
+            /*string inData = currentSerialPort.ReadLine();
+            string[] datos = inData.Split(',');
+            label1.Text = datos[0];
+            label2.Text = datos[1];
+            label3.Text = datos[2];
+            */
+            //Cuenta el número de columnas
+
+            tmrAdquisicion.Start(); //Iniciamos el timer para escribir en el dataGriedView
+
+        }
+
+        /// <summary>
+        /// Función que imprime los datos en un dataGriedView
+        /// </summary>
+        /// <param name="inData">Cadena con datos separados por una coma</param>
+        private void imprimirDatos(string inData)
+        {
+            string[] datos = inData.Split(',');
+            dataGridView1.Rows.Add(datos); //Agregamos los datos a una fila nueva del dataGridView
+        }
+
+        /// <summary>
+        /// Función de un reloj para actualizar un dataGriedView con datos leídos en tiempo real
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tmrAdquisicion_Tick(object sender, EventArgs e)
+        {
+            // dt.Rows.Add(time, lbSpd.Text, lbAlt.Text, lbLat.Text, lbLng.Text);
+            string inData = serialPort.ReadLine(); //Guardamos los datos leídos del puerto en una cadena
+            int columnLength = inData.Split(',').Length; //Guardamos el # de datos recibidos por lectura para conocer el # de columnas
+            dataGridView1.ColumnCount = columnLength;  //Fijamos el número de columnnas que habrá por lectura
+            imprimirDatos(inData); //Se llama la función para imprimir en un dataGriedView
+        }
+
+        #region Botones
+        /// <summary>
+        /// Botón que activa la función ComunicacionPuertoSeria para comenzar la lectura e impresión de datos
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            ComunicacionPuertoSerie();
+        }
+
+        /// <summary>
+        /// Botón que cierra el puerto con la comunicación serial
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+        }
+        #endregion
+
+        
+    }
+}
